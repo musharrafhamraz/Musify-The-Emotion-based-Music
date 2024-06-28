4554f070e69c4bfff8e7c675c7952f8bfd99506e
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-06-28 12:17:32 +0500

    Trying to build the project.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 
 // babel.config.js
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    api.cache(true);
+    return {
+      presets: ["babel-preset-expo"],
+      plugins: ["nativewind/babel"],
+      plugins: ['@babel/plugin-syntax-jsx'],
+    };
   };
-};
